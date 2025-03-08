52afcc0f785180363d710fb19961081ca0890619
Sayyad Ali <qadriali54@gmail.com>
2025-03-08 11:35:02 +0530

    isOwner and isReviewAuthor

--- middleware.js
+++ middleware.js
@@ -1,8 +1,13 @@
+const Listing = require("./models/listing");
+const Review = require("./models/review");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+
 module.exports.isLoggedIn = (req, res, next) => {
     //console.log(req.path, "..", req.originalUrl);//path is relative // original path is absolute
     if (!req.isAuthenticated()) {
         req.session.redirectUrl = req.originalUrl;
-        req.flash("error", "You must be logged in to create list");
+        req.flash("error", "You are not the owner of this listing");
         return res.redirect("/login");
     }
     next();
@@ -11,6 +16,48 @@ module.exports.isLoggedIn = (req, res, next) => {
 module.exports.savedRedirectUrl = (req, res, next) => {
     if (req.session.redirectUrl) {
         res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+};
+
+module.exports.isOwner = async (req, res, next) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    if (!listing.owner._id.equals(res.locals.currUser._id)) {
+        req.flash("error", "You don't have permission to edit");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+};
+
+module.exports.validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
+module.exports.validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    let { id, reviewId } = req.params;
+    let review = await Review.findById(reviewId);
+    if (!review.author._id.equals(res.locals.currUser._id)) {
+        req.flash("error", "You didn't create this review");
+        return res.redirect(`/listings/${id}`);
     }
     next();
 };
