263c622e27455db843898be0b64bd11c084e0b4e
Sayyad Ali <qadriali54@gmail.com>
2025-03-09 14:27:05 +0530

    add image url and filename to DB

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,3 +1,4 @@
+const { connect } = require("mongoose");
 const Listing = require("../models/listing");
 
 module.exports.index = async (req, res) => {
@@ -9,8 +10,12 @@ module.exports.renderNewForm = (req, res) => {
     return res.render("listings/new.ejs");
 };
 module.exports.createListing = async (req, res, next) => {
+    let url = req.file.path;
+    let filename = req.file.filename;
+    console.log(url, "and", filename);
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = { url, filename };
     await newListing.save();
     req.flash("success", "New Listing Created");
     res.redirect("/listings");
@@ -47,20 +52,14 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateListing = async (req, res) => {
     let { id } = req.params;
-    let listing = await Listing.findById(id);
-    /*  if (!req.body.listing) {
-         throw new ExpressError(400, "send some valid data for listing");
-     } */
-    let { image, ...updatedData } = req.body.listing; // Extract image field separately
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
 
-    // If user provides a new image URL, update it; otherwise, keep the existing one
-    if (image && image.trim() !== "") {
-        updatedData.image = { filename: "listingimage", url: image };
-    } else {
-        updatedData.image = listing.image; // Preserve the old image if none is provided
+    if (typeof req.file !== "undefined") {
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = { url, filename };
+        await listing.save();
     }
-
-    await Listing.findByIdAndUpdate(id, updatedData, { new: true });
     req.flash("success", "Listing Updated");
     res.redirect(`/listings/${id}`);
 };
