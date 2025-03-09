9ceadb7e285e6f922de742c236169967ec97ee56
Sayyad Ali <qadriali54@gmail.com>
2025-03-09 14:29:22 +0530

    used MULTER and cloudinary

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,9 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
+const multer = require('multer');
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 //index and create
 router
@@ -11,7 +14,9 @@ router
     .get(wrapAsync(listingController.index))
     .post(
         isLoggedIn,
+        upload.single('listing[image]'),
         validateListing,
+
         wrapAsync(listingController.createListing)
     );
 
@@ -27,6 +32,7 @@ router
     .put(
         isLoggedIn,
         isOwner,
+        upload.single('listing[image]'),
         validateListing,
         wrapAsync(listingController.updateListing)
     )
